0a16d5bf96d6040865ee271201a8af0d893c9e4f
bing <chengbingx@163.com>
2017-10-26 15:59:23 +0800

    规范“系统初始化”代码

--- research/object_detection/server.py
+++ research/object_detection/server.py
@@ -45,13 +45,6 @@ NUM_CLASSES = 90
 
 max_image_size = 640
 confidence_level = 0.6
-redirect_html = ''
-image_tensor = ''
-detection_boxes = ''
-detection_scores = ''
-detection_classes = ''
-num_detections = ''
-tf_sess = ''
 
 
 # Load a (frozen) Tensorflow model into memory.
@@ -66,6 +59,20 @@ with detection_graph.as_default():
         # tf.summary.FileWriter("logdir", detection_graph)
 
 
+# Init
+# Definite input and output Tensors for detection_graph
+image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+# Each box represents a part of the image where a particular object was detected.
+detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+# Each score represent how level of confidence for each of the objects.
+# Score is shown on the result image, together with the class label.
+detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+# Create session
+tf_sess = tf.Session(graph=detection_graph)
+
+
 # Loading label map
 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
@@ -76,19 +83,6 @@ category_index = label_map_util.create_category_index(categories)
 def load_image_into_numpy_array(image):
     (im_width, im_height) = image.size
     return np.array(image.getdata()).reshape((im_height, im_width, 3)).astype(np.uint8)
-
-
-with detection_graph.as_default():
-    with tf.Session(graph=detection_graph) as tf_sess:
-        # Definite input and output Tensors for detection_graph
-        image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-        # Each box represents a part of the image where a particular object was detected.
-        detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-        # Each score represent how level of confidence for each of the objects.
-        # Score is shown on the result image, together with the class label.
-        detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
-        detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
-        num_detections = detection_graph.get_tensor_by_name('num_detections:0')
 
 
 class Box:
